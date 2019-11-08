ec3334277151b67c238b2cef3392afed6e993c64
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-08 15:21:17 +0000

    probably the final update on the reccomended column

--- src/Recommended.js
+++ src/Recommended.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Recommended.css'
 import ReactDOM from 'react-dom'
-import {FaCog, FaPersonBooth} from 'react-icons/fa'
+import {FaCog, FaSearch} from 'react-icons/fa'
 import Cropd1 from './cropd1.png'
 import Cropd2 from './cropd2.png'
 import Cropd3 from './cropd3.png'
@@ -11,66 +11,67 @@ function Recommended() {
     <div className="Recommended">
         <div id="recInner">
           <div id="searchTwitter">
-            <input type="text"/>
+            <input type="text" placeholder="Search"/>
+            <FaSearch className="searchIcon"/>
           </div>
           <div id="trendsForYou">
             <div className="recHead">
               <h1>Trends for you <a href=""><FaCog className="cog"/></a></h1>
             </div>
-            <div className="trend">
+            <a href="#"><div className="trend">
               <p className="trendType">Trending in video games</p>
-              <p className="hashTag"><a href="#">#Dewritos</a></p>
+              <p className="hashTag">#Dewritos</p>
               <p className="tweetNumber">751, 995 Tweets</p>
-            </div>
-            <div className="trend">
+            </div></a>
+            <a href="#"><div className="trend">
               <p className="trendType">Trending in the UK</p>
-              <p className="hashTag"><a href="#">#MoaningAboutTea</a></p>
+              <p className="hashTag">#MoaningAboutTea</p>
               <p className="tweetNumber">555, 123 Tweets</p>
-            </div>
-            <div className="trend">
+            </div></a>
+            <a href="#"><div className="trend">
               <p className="trendType">Trending worldwide</p>
-              <p className="hashTag"><a href="#">#BiggestPlanetOnEarth</a></p>
+              <p className="hashTag">#BiggestPlanetOnEarth</p>
               <p className="tweetNumber">999, 666 Tweets</p>
-            </div>
-            <div className="trend">
+            </div></a>
+            <a href="#"><div className="trend">
               <p className="trendType">Trending in music</p>
-              <p className="hashTag"><a href="#">#ClassicalHardPunkJazz</a></p>
+              <p className="hashTag">#ClassicalHardPunkJazz</p>
               <p className="tweetNumber">123, 246 Tweets</p>
-            </div>
-            <div className="trend">
+            </div></a>
+            <a href="#"><div className="trend">
               <p className="trendType">Promoted by Mr Guy</p>
-              <p className="hashTag"><a href="#">#GuyProducts</a></p>
+              <p className="hashTag">#GuyProducts</p>
               <p className="tweetNumber">Buy all the things!</p>
-            </div>
-            <div className="recFoot">
-              <p className="showMore"><a href="#">Show more</a></p>
-            </div>
+            </div></a>
+            <a href="#"><div className="recFoot">
+              <p className="showMore">Show more</p>
+            </div></a>
           </div>
           <div id="whoToFollow">
             <div className="recHead">
               <h1>Who to follow</h1>
             </div>
-            <div className="followable">
+            <a href="#"><div className="followable">
               <p><img src={Cropd1} className="followableIcon"/></p>
               <p className="twitterer">Micheal Mild</p>
               <p className="twitterHandle">#MilqueToastMike</p>
               <p className="followButton"><a href="#">Follow</a></p>
-            </div>
-            <div className="followable">
+            </div></a>
+            <a href="#"><div className="followable">
               <p><img src={Cropd2} className="followableIcon"/></p>
               <p className="twitterer">Montey Money</p>
               <p className="twitterHandle">#Burnsey</p>
               <p className="followButton"><a href="#">Follow</a></p>
-            </div>
-            <div class="followable">
+            </div></a>
+            <a href="#"><div class="followable">
               <p><img src={Cropd3} className="followableIcon"/></p>
               <p className="twitterer">Mr Macho</p>
               <p className="twitterHandle">#KittyWhisperer</p>
               <p className="followButton"><a href="#">Follow</a></p>
-            </div>
-            <div className="recFoot">
-              <p className="showMore"><a href="#">Show more</a></p>
-            </div>
+            </div></a>
+            <a href="#"><div className="recFoot">
+              <p className="showMore">Show more</p>
+            </div></a>
           </div>
         </div>
     </div>
